6e77c751b48f4c3998c0f601b0c72963041d5837
Fabio Ono Tavoloni <fabioono25@gmail.com>
2021-03-30 21:11:12 +0100

    first commit

---
+++ design-patterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace design_patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
