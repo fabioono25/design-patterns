c1716e7face14266c0b429871a35b343b0ca6c3d
Fabio Ono Tavoloni <fabioono25@gmail.com>
2022-10-19 20:19:01 +0200

    template method implementation

---
+++ design-patterns/HeadFirst/9-TemplateMethod/CaffeineBeverageWithHook.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+
+namespace design_patterns.HeadFirst.TemplateMethod
+{
+    public abstract class CaffeineBeverageWithHook
+    {
+        public void prepareRecipe()
+        {
+            boilWater();
+            brew();
+            pourInCup();
+            if (customerWantsCondiments())
+            {
+                addCondiments();
+            }
+        }
+
+        public abstract void brew();
+
+        public abstract void addCondiments();
+
+        void boilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+
+        void pourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+
+        public bool customerWantsCondiments()
+        {
+            return true;
+        }
+    }
+
+    public class CoffeeWithHook: CaffeineBeverageWithHook
+    {
+        public override void brew()
+        {
+            Console.WriteLine("Dripping Coffee through filter");
+        }
+
+        public override void addCondiments()
+        {
+            Console.WriteLine("Adding Sugar and Milk");
+        }
+
+        public bool customerWantsCondiments()
+        {
+
+            String answer = getUserInput();
+
+            if (answer.ToLowerInvariant().StartsWith("y"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private String getUserInput()
+        {
+            String answer = null;
+
+            Console.WriteLine("Would you like milk and sugar with your coffee (y/n)? ");
+
+            try
+            {
+                answer = Console.ReadLine();
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine("IO error trying to read your answer");
+            }
+            if (answer == null)
+            {
+                return "no";
+            }
+            return answer;
+        }
+    }
+
+    public class TeaWithHook: CaffeineBeverageWithHook
+    {
+
+
+    public override void brew()
+    {
+        Console.WriteLine("Steeping the tea");
+    }
+
+    public override void addCondiments()
+    {
+        Console.WriteLine("Adding Lemon");
+    }
+
+    public bool customerWantsCondiments()
+    {
+
+        String answer = getUserInput();
+
+        if (answer.ToLowerInvariant().StartsWith("y"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private String getUserInput()
+    {
+        // get the user's response
+        String answer = null;
+
+        Console.WriteLine("Would you like lemon with your tea (y/n)? ");
+
+        try
+        {
+            answer = Console.ReadLine();
+        }
+        catch (IOException ioe)
+        {
+            Console.WriteLine("IO error trying to read your answer");
+        }
+        if (answer == null)
+        {
+            return "no";
+        }
+        return answer;
+    }
+}
+}
+
---
+++ design-patternsTests/HeadFirst/9-TemplateMethod/TemplateMethodTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using design_patterns.HeadFirst.TemplateMethod;
+using Xunit;
+
+namespace design_patternsTests.HeadFirst.TemplateMethod
+{
+    public class TemplateMethodTests
+    {
+        [Fact]
+        public void WorkingWithFacade()
+        {
+            TeaWithHook teaHook = new TeaWithHook();
+            CoffeeWithHook coffeeHook = new CoffeeWithHook();
+
+            Console.WriteLine("\nMaking tea...");
+            teaHook.prepareRecipe();
+
+            Console.WriteLine("\nMaking coffee...");
+            coffeeHook.prepareRecipe();
+        }
+    }
+}
+
